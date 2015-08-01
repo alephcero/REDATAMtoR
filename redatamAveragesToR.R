0cd8e5c00ad146e4580f2e1b36729c0fd86a3dce
pipegonz <felipegonz@gmail.com>
2015-08-01 20:26:31 -0300

    function to read ascii files exported wit redatam

---
+++ redatamAveragesToR.R
@@ -0,0 +1,53 @@
+library(reshape)
+# This function takes an ASCII redatam exported text file with averages of a variable with RADIOS as areabreaks
+# and returns an R data.frame object
+
+redatamAverages = function(archivo) {
+  
+  #Read files in ASCII format
+  data = readLines(archivo)
+  data = iconv(data, "latin1", "utf-8")
+  
+  #Removes any empty tables
+  posicionTablasVacias = grep("Tabla",data)
+  data = data[-(posicionTablasVacias-2)]
+  
+  #Creates a data.frame with the areas
+  posicionAreas = grep("AREA",data)
+  areas = data[posicionAreas]
+  areas = as.data.frame(areas)
+  areas = colsplit(areas$areas,"\t",c("vacio1","area"))
+  areas = as.data.frame(areas[,2])
+  
+  #Gets the data set for the variable ante the amount of households
+  posicionDatos = grep("Total y Promedio",data)
+  datos = data[posicionDatos]
+  datos = gsub("\t100,00 %","",datos)
+  datos = gsub("\t Total y Promedio\t","",datos)
+  datos = gsub(",",".",datos)
+  
+  #separates the data set with resumes the total of households
+  datosResumen = datos[length(datos)]
+  datosResumen = as.data.frame(datosResumen)
+  datos = as.data.frame(datos[-length(datos)])
+  
+  datos = colsplit(datos$datos,"\t",c("cantidad","capeco"))
+  datosResumen = colsplit(datosResumen$datos,"\t",c("cantidad","capeco"))
+  datosResumen$cantidad = as.character(datosResumen$cantidad)
+  datosResumen$cantidad = gsub(".","",datosResumen$cantidad,fixed = TRUE)
+  
+  #check for the sum of households in the table adds up to the resume table
+  check1 = sum(datos$cantidad) == datosResumen$cantidad
+  #checks that both tables have the same amount of rows
+  check2 = nrow(areas)==nrow(datos)
+  
+  if(check1 & check2){
+    base = cbind(areas,datos)
+  } else {
+    stop("ATENCIÓN: La suma de los radios NO coincide con los totales en la tabla resumen o no son la misma cantidad de areas y datos")
+  }
+  
+  
+}
+
+
